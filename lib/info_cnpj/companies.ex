c2dabf4df404a27425727d04153586d2ba835aa2
MauricioMSoares <mauricio.soares@sptech.school>
2023-11-15 11:59:41 -0300

    feat: organized project structure to use a controller

--- lib/info_cnpj/companies.ex
+++ lib/info_cnpj/companies.ex
@@ -110,4 +110,47 @@ defmodule InfoCnpj.Companies do
     company
     |> Repo.insert()
   end
+
+  def create_company_struct_from_json(data) do
+    case data do
+      {:ok, data} ->
+        %Company{
+          cep: access_estabelecimento(data)["cep"],
+          cnpj: access_estabelecimento(data)["cnpj"],
+          company_type: access_estabelecimento(data)["tipo"],
+          country: Map.get(access_estabelecimento(data), "pais", %{})["nome"],
+          county: Map.get(access_estabelecimento(data), "cidade", %{})["nome"],
+          district: access_estabelecimento(data)["bairro"],
+          email: access_estabelecimento(data)["email"],
+          enterprise_name: data["razao_social"],
+          fantasy_name: access_estabelecimento(data)["nome_fantasia"],
+          number: access_estabelecimento(data)["numero"],
+          opening_date: access_estabelecimento(data)["data_inicio_atividade"],
+          phone_ddd: access_estabelecimento(data)["ddd1"],
+          phone_number: access_estabelecimento(data)["telefone1"],
+          public_place: access_estabelecimento(data)["logradouro"],
+          public_place_type: access_estabelecimento(data)["tipo_logradouro"],
+          state: Map.get(access_estabelecimento(data), "estado", %{})["nome"]
+        }
+
+      _ ->
+        IO.puts("An error occurred while attempting to create company struct.")
+    end
+  end
+
+  defp access_estabelecimento(json) do
+    Map.get(json, "estabelecimento", %{})
+  end
+
+  def get_company_from_api(cnpj) do
+    case HTTPoison.get!(
+           "https://publica.cnpj.ws/cnpj/#{cnpj}"
+         ) do
+      %HTTPoison.Response{body: body, status_code: 200} ->
+        {:ok, Jason.decode!(body)}
+
+      %HTTPoison.Response{status_code: 404} ->
+        {:error, :not_found}
+    end
+  end
 end
--- lib/info_cnpj_web/live/verify_cnpj_live.ex
+++ lib/info_cnpj_web/live/verify_cnpj_live.ex
@@ -31,8 +31,8 @@ defmodule InfoCnpjWeb.VerifyCnpjLive do
         {:noreply, assign(socket, :company, company)}
 
       _ ->
-        data = get_company_from_api(cnpj)
-        company = create_company_struct_from_json(data)
+        data = Companies.get_company_from_api(cnpj)
+        company = Companies.create_company_struct_from_json(data)
         Companies.create_retrieved_company(company)
 
         IO.inspect("Trouxe de lá da API")
@@ -46,52 +46,9 @@ defmodule InfoCnpjWeb.VerifyCnpjLive do
     attrs |> Map.get("cnpj")
   end
 
-  defp get_company_from_api(cnpj) do
-    case HTTPoison.get!(
-           "https://publica.cnpj.ws/cnpj/#{cnpj}"
-         ) do
-      %HTTPoison.Response{body: body, status_code: 200} ->
-        {:ok, Jason.decode!(body)}
-
-      %HTTPoison.Response{status_code: 404} ->
-        {:error, :not_found}
-    end
-  end
-
-  defp create_company_struct_from_json(data) do
-    case data do
-      {:ok, data} ->
-        %Company{
-          cep: access_estabelecimento(data)["cep"],
-          cnpj: access_estabelecimento(data)["cnpj"],
-          company_type: access_estabelecimento(data)["tipo"],
-          country: Map.get(access_estabelecimento(data), "pais", %{})["nome"],
-          county: Map.get(access_estabelecimento(data), "cidade", %{})["nome"],
-          district: access_estabelecimento(data)["bairro"],
-          email: access_estabelecimento(data)["email"],
-          enterprise_name: data["razao_social"],
-          fantasy_name: access_estabelecimento(data)["nome_fantasia"],
-          number: access_estabelecimento(data)["numero"],
-          opening_date: access_estabelecimento(data)["data_inicio_atividade"],
-          phone_ddd: access_estabelecimento(data)["ddd1"],
-          phone_number: access_estabelecimento(data)["telefone1"],
-          public_place: access_estabelecimento(data)["logradouro"],
-          public_place_type: access_estabelecimento(data)["tipo_logradouro"],
-          state: Map.get(access_estabelecimento(data), "estado", %{})["nome"]
-        }
-
-      _ ->
-        IO.puts("An error occurred while attempting to create company struct.")
-    end
-  end
-
-  defp access_estabelecimento(json) do
-    Map.get(json, "estabelecimento", %{})
-  end
-
   @cnpj_regex ~r/(\d{2})\.?(\d{3})\.?(\d{3})\/?(\d{4})-?(\d{2})/
 
-  def extract_cnpj_number(cnpj) do
+  defp extract_cnpj_number(cnpj) do
     case Regex.match?(@cnpj_regex, cnpj) do
       true ->
         Regex.replace(@cnpj_regex, cnpj, "\\1\\2\\3\\4\\5")
